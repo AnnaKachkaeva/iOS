32f6f8e574c2ca6a9387944aca2902392464843c
Anna Kachkaeva <kachkaevaanna@gmail.com>
2021-07-13 15:28:19 +0300

    Much better with Smaug Magnificent

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -15,9 +15,26 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showMessage(sender: UIButton) {
-        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
-        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-        present(alertController, animated: true, completion: nil)
+        
+        let dict: [String:String] = ["🦷":"My teath are swords",
+                                     "💅🏼":"my claws spears",
+                                     "🦋":"my wings a hurricane",
+                                     "🐉":"I'm fire, I'm death"]
+        //the sender is the button whitch is selected by the user
+        //we store the sender in constant
+        let selectedButton = sender
+        
+        //get emoji from typed button
+        if let LookUp = selectedButton.titleLabel?.text{
+            let text : String? = dict[LookUp]
+        
+        
+            let alertController = UIAlertController(title: "Your Smaug said:", message:text, preferredStyle: UIAlertController.Style.alert)
+            
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            
+            present(alertController, animated: true, completion: nil)
+        }
     }
 
 
