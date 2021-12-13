95010e4e2e448413ece031b6696225a7af85f8a9
AnnaKachkaeva <63503721+AnnaKachkaeva@users.noreply.github.com>
2021-12-13 10:49:59 +0300

    Add files via upload

---
+++ Checklist/Checklist/ChecklistItem.swift
@@ -0,0 +1,14 @@
+//
+//  ChecklistItem.swift
+//  Checklist
+//
+//  Created by Анна Качкаева on 28.11.2021.
+//
+
+import Foundation
+
+
+class ChecklistItem{
+    var text = ""
+    var checked = false
+}
--- Checklist/Checklist/ChecklistViewController.swift
+++ Checklist/Checklist/ChecklistViewController.swift
@@ -11,28 +11,33 @@ import UIKit
 //FIXME: fghjkl
 
 class ChecklistViewController: UITableViewController {
-    var row0checked = true
-    var row1checked = true
-    var row2checked = true
-    var row3checked = true
-    var row4checked = true
+    var items : [ChecklistItem] = []
+   
 
 
 
 
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 50
+        let cellsAmount = 50
+        
+        for i in 0..<cellsAmount {
+            let item = ChecklistItem()
+            item.text = "i am \(i) element here"
+            items.append(item)
+        }
+        
+        return cellsAmount
     }
+    
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
         let label = cell.viewWithTag(1000) as! UILabel
-        
-        
-        label.text = " row  number \(indexPath[1])"
-        
-        
+                
+        let item = items[indexPath[1]]
+        label.text = item.text
+        configureCheckmark(for: cell, with: item)
         
         return cell
     }
@@ -43,45 +48,23 @@ class ChecklistViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        //написать функцию которая запоминает значения для первых 5 клеток
-        
-        var isChecked = false
-        
         if let cell = tableView.cellForRow(at: indexPath) { // checkes if there is cell tapped
-           
-            switch indexPath.row {
-            case 0:
-                row0checked.toggle()
-                isChecked = row0checked
-            case 1:
-                row1checked.toggle()
-                isChecked = row1checked
-            case 2:
-                row2checked.toggle()
-                isChecked = row2checked
-            case 3:
-                row3checked.toggle()
-                isChecked = row3checked
-            case 4:
-                row4checked.toggle()
-                isChecked = row4checked
-                
-            default:
-                isChecked = false
-            }
-            
-            
-            if isChecked { //if there is no checkmark
-                cell.accessoryType = .checkmark //let it be
-            } else { //if there is one
-                cell.accessoryType = .none //errase it
-            }
+            items[indexPath[1]].checked.toggle()
+            configureCheckmark(for: cell, with: items[indexPath[1]])
         }
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
 
+    func configureCheckmark(for cell: UITableViewCell, with item: ChecklistItem){
+
+        if item.checked {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+
+    }
 }
 
